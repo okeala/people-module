4d35b845f53794176014844757be1164bb0f80e9
Steph <stephane.maissin@gmail.com>
2025-11-12 15:18:48 +0000

    WIP: sauvegarde avant subtree pull

--- app/Providers/PeopleServiceProvider.php
+++ app/Providers/PeopleServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace Modules\People\Providers;
 
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Modules\Helpcenter\Models\Faq;
+use Modules\Helpcenter\Models\Tag;
+use Modules\People\Models\Person;
 use Nwidart\Modules\Traits\PathNamespace;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
@@ -27,6 +31,10 @@ class PeopleServiceProvider extends ServiceProvider
         $this->registerConfig();
         $this->registerViews();
         $this->loadMigrationsFrom(module_path($this->name, 'database/migrations'));
+
+        Relation::enforceMorphMap([
+            'person'     => Person::class,
+        ]);
     }
 
     /**
