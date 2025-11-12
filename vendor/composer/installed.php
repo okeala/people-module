0917a1edf8a28f0b9d07ba42e4bb317156447e1a
Steph <stephane.maissin@gmail.com>
2025-11-12 15:07:02 +0000

    Update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'okeala/people-module',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'be7b2d6043ed00d0f77a70bcf2c2bb0624aafb79',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'okeala/people-module' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'be7b2d6043ed00d0f77a70bcf2c2bb0624aafb79',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
